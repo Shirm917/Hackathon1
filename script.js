ba8976262d5b8bcda9f421b3b783b21e5ae7c09d
Shirm917 <shirm91799@gmail.com>
2022-11-01 06:07:05 +0200

    finsihed part one of this feature

--- script.js
+++ script.js
@@ -19,13 +19,15 @@ function addColors() {
 addColors();
 
 // Adding event listener of when you click the function hideAndShow happens
-function addVisibilitys(listElements) {
+function addEvents(listElements) {
     for (const elem of listElements) {
         elem.addEventListener("click", hideAndShow);
+        elem.addEventListener("click", matchCheck1);
+        elem.addEventListener("click", matchCheck2);
     }
 }
 
-addVisibilitys(backs); // adding the event listener to all the backfaces
+addEvents(backs); // adding event listeners to all the backfaces
 // addVisibilitys(fronts);
 
 // When you click on a card the class hide is toggled on and off and when on the card invisible
@@ -33,5 +35,39 @@ function hideAndShow(event) {
     event.target.classList.toggle("hide");
     // event.target.previousElementSibling.classList.toggle("hide");
 }
+
+// getting the color of the first card 
+let colorOne;
+let cardOne;
+function matchCheck1(event) {
+    colorOne = event.target.nextElementSibling.style.background;
+    cardOne = event.target;
+    // event.target.removeEventListener("click", matchCheck1);
+    for (const back of backs) {
+        back.removeEventListener("click", matchCheck1);
+        back.addEventListener("click", matchClear);
+    }
+}
+
+// getting the color of the second card
+let colorTwo;
+let cardTwo;
+function matchCheck2(event) {
+    colorTwo = event.target.nextElementSibling.style.background;
+    cardTwo = event.target;
+}
+
+// clearing the matches
+function matchClear(event) {
+    if (colorOne === colorTwo) {
+        console.log("yes");
+        cardOne.nextElementSibling.classList.toggle("hide");
+        cardTwo.nextElementSibling.classList.toggle("hide");
+    } else {
+        cardOne.classList.toggle("hide");
+        cardTwo.classList.toggle("hide");
+    }
+}
+
 
 
